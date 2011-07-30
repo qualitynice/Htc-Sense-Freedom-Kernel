feaa9933d46cae236b5f3d14d1630617ef8ea627
lithid <lithid@WorkBot.(none)>
2011-07-30 12:00:45 -0400

    Cleaning things up a bit.

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 7 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 PREEMPT Fri Jul 29 14:09:01 EDT 2011"
-#define LINUX_COMPILE_TIME "14:09:01"
-#define LINUX_COMPILE_BY "lithid"
-#define LINUX_COMPILE_HOST "WorkBot"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.35.10v0.1"
